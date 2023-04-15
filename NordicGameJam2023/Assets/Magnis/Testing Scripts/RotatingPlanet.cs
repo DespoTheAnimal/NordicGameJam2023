79531ae95df2500e95045cea746b920edb09fb73
DespoTheAnimal <thedanishmagnus@gmail.com>
2023-04-15 09:59:25 +0200

    Added script for rotating player around anchor for PoC

---
+++ NordicGameJam2023/Assets/Magnis/Testing Scripts/RotatingPlanet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlanet : MonoBehaviour
+{
+    [SerializeField] GameInput _GI;
+
+    //public GameObject anchor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void RotatePlanet()
+    {
+        Vector2 readValues = _GI.GetMovement();
+        Vector3 inputValues = new Vector3(readValues.x, 0, readValues.y);
+
+        Quaternion rotation = Quaternion.Euler(inputValues.z, 0, -inputValues.x);
+        transform.rotation *= rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlanet();
+    }
+}
