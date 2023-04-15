6610dc2858fbb22c0ece7c8e6ebcabca58c60d75
DespoTheAnimal <thedanishmagnus@gmail.com>
2023-04-15 09:42:38 +0200

    Added some movement, controller included

---
+++ NordicGameJam2023/Assets/Scripts/Core/GameInput.cs
@@ -0,0 +1,28 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    PlayerInputMap PlayerInput;
+
+
+    private void Awake()
+    {
+        PlayerInput = new PlayerInputMap();
+    }
+
+    private void OnEnable()
+    {
+        PlayerInput.Enable();
+    }
+
+    public Vector2 GetMovement()
+    {
+        Vector2 readValue = PlayerInput.Controls.Movement.ReadValue<Vector2>().normalized;
+
+        return readValue;
+    }
+}
--- NordicGameJam2023/Assets/Scripts/PlayerController.cs
+++ NordicGameJam2023/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameInput _GI;
+
+    public int speedModifier = 0;
+
+    private Rigidbody _RB;
+    private void Start()
     {
-        
+        _RB = GetComponent<Rigidbody>();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 readValues = _GI.GetMovement();
+        Vector3 inputValues = new Vector3(readValues.x, 0, readValues.y);
+
+
+        _RB.MovePosition(transform.position + inputValues * speedModifier * Time.deltaTime);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        HandleMovement();
     }
 }
