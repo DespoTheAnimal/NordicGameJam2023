543358b0f70a6fc535a873db1c69688c3d023642
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2023-04-15 12:24:31 +0200

    Rotating

---
+++ NordicGameJam2023/Assets/Marius/Rotating.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public int control;
+    public GameObject player;
+
+    [SerializeField] GameInput _GI;
+
+    public float rotationSpeed;
+
+    //public GameObject anchor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void RotatePlanet()
+    {
+        Vector2 readValues = _GI.GetMovement();
+        Vector3 inputValues = new Vector3(readValues.x, 0, readValues.y);
+
+
+
+        if (control == 1)
+        {
+            Quaternion rotation = Quaternion.Euler(inputValues.z * rotationSpeed, 0, 0/*inputValues.x *rotationSpeed*/);
+            transform.rotation *= rotation;
+        }
+        else if(control == 2)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, inputValues.x *rotationSpeed);
+            transform.rotation *= rotation;
+        }
+ 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RotatePlanet();
+    }
+}
