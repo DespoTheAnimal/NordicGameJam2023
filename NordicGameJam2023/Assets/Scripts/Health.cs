894dd186d2b297408cda13843e7dabfc412d3782
vkingen <lassevikingkristensen@gmail.com>
2023-04-16 12:57:52 +0200

    asd

--- NordicGameJam2023/Assets/Scripts/Health.cs
+++ NordicGameJam2023/Assets/Scripts/Health.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health = 100;
+
+    public Image healthBar;
     public void TakeDamage(int amount)
     {
         health -= amount;
-        if(health <= 0)
+        healthBar.fillAmount = health / 100f;
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
